2ba3c7a5039dbf8442adee254872496d38276e0e
MTP84 <metalpen1984@users.noreply.github.com>
2018-10-14 01:50:12 +0800

    Upload auto-setup.sh, current state
    not test yet

---
+++ back_to_user.sh
@@ -0,0 +1,25 @@
+#!/usr/bash
+sudo passwd
+
+# installing necessary programs
+sudo apt update
+sudo apt upgrade
+sudo apt install audacious vlc hime hime-anthy pcmanfm terminator vim geany
+
+sudo apt install pcmanx-gtk2 gimp
+
+# installing super flat icons
+sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo apt-get update
+sudo apt-get install flat-remix
+
+# configuring imput method
+sudo im-config
+
+# install libreoffice 
+sudo apt install libreoffice-texmaths jabref libreoffice-style-sifr 
+
+# install python stuff
+sudo apt install python3-pip 
+pip3 install ipython matplotlib numpy scipy ipython jupyter
+
