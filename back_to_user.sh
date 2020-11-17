5a9ca94143206e6885a373a0c6685d2b56ed8041
MTP84 <metalpen1984@users.noreply.github.com>
2020-11-17 10:34:44 +0100

    Update back_to_user.sh

--- back_to_user.sh
+++ back_to_user.sh
@@ -31,7 +31,9 @@ sudo apt install libreoffice-texmaths jabref libreoffice-style-sifr pdfshuffler
 
 # install python stuff
 sudo apt install python3-pip 
-pip3 install ipython matplotlib numpy scipy jupyter networkx jupyterlab scikit-learn mpi4py
+# install python stuff for science task
+pip3 install ipython matplotlib numpy scipy jupyterlab
+pip3 install networkx scikit-learn mpi4py netcdf4
 
 # install for art
 sudo apt install gimp mypaint mypaint-brushes pikopixel.app inkscape inkscape-open-symbols
@@ -43,4 +45,5 @@ sudo apt install inkscape inkscape-open-symbols
 sudo apt install okular
 sudo apt install evolution evolution-ews
 
+# installation for publication work / german environment
 sudo apt install texlive gummi texlive-publishers texlive-lang-german 
